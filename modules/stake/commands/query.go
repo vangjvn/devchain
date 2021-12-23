bbfb538c902951b8b46e61c2f860f54ae91c2446
WangJun <75415837+vangjvn@users.noreply.github.com>
2021-12-23 17:40:12 +0800

    Update query.go

--- modules/stake/commands/query.go
+++ modules/stake/commands/query.go
@@ -2,7 +2,7 @@ package commands
 
 import (
 	"fmt"
-	"github.com/second-state/devchain/sdk/client/commands"
+	"github.com/vangjvn/devchain/sdk/client/commands"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
